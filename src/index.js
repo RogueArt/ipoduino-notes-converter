22c65e2dbf971599575af3e7eea0635010c98b61
RogueArt <57082175+RogueArt@users.noreply.github.com>
2021-01-20 23:58:20 -0800

    Fixed rollback from github

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import copyToClipboard from "./clipboard.js";
 import processFile from "./file.js";
 import parse from "./parse.js";
 
-console.log("test");
 document.getElementById("copy-to-clipboard").addEventListener("click", () => {
   // Get processed code's latest value
   const processedCode = document.getElementById("output").value;
@@ -11,15 +10,17 @@ document.getElementById("copy-to-clipboard").addEventListener("click", () => {
 
 // Listen for file uploads
 const fileInput = document.getElementById("file-input");
-fileInput.addEventListener("change", async () => {
-  // Get contents of uploaded file
-  const file = fileInput.files[0];
-  console.log(file);
-  const contents = await processFile(file);
-  // Turn the contents into code
-  const processedCode = parse(contents);
-  displayOutput(processedCode);
-});
+fileInput.addEventListener(
+  "change",
+  async () => {
+    // Get contents of uploaded file
+    const contents = await processFile(fileInput.files[0]);
+    // Turn the contents into code
+    const processedCode = parse(contents);
+    displayOutput(processedCode);
+  },
+  false
+);
 
 // Helper to display contents to the page
 function displayOutput(outputCode) {
